2d4590b61294218e068ab5b09d065267dde42ca0
tyash3011 <41552851+tyash3011@users.noreply.github.com>
2022-03-12 17:18:48 -0500

    second commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -3,12 +3,19 @@ package controllers;
 import play.mvc.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import views.html.*;
+import play.data.*;
 
+import javax.inject.Inject;
+import play.mvc.Http.Request;
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+
+	@Inject
+	FormFactory formFactory;
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -18,7 +25,17 @@ public class HomeController extends Controller {
      */
 	public CompletionStage<Result> index() {
 		
-        return CompletableFuture.completedFuture(ok(index.render()));
+        return CompletableFuture.completedFuture(ok(views.html.index.render(" ")));
     }
-
+	
+	public CompletionStage<Result> Search(Http.Request request) {
+		
+		final String query = formFactory.form().bindFromRequest(request).get("query");
+		return CompletableFuture.completedFuture(ok(views.html.index.render(query)));
+		
+		
+    }
+		
 }
+
+
